2cd6bf2a475cad4a40d95fe889a0df34a06d9d86
Wampe <4699636+Wampe@users.noreply.github.com>
2022-02-25 15:32:03 +0100

    Refactoring Firebase service Reimplemented bot call

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,7 @@
 import dotenv from 'dotenv';
-import { initializeApp } from 'firebase/app';
-import { getFirestore, collection, getDocs } from 'firebase/firestore'
+import { TmiService } from './services/tmi.service';
 
 dotenv.config();
 
-// Import the functions you need from the SDKs you need
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: process.env?.FIREBASE_API_KEY,
-  authDomain: process.env?.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env?.FIREBASE_PROJECT_ID,
-  storageBucket: process.env?.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env?.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env?.FIREBASE_APP_ID
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-const server = async () => {
-	const querySnapshot = await getDocs(collection(db, 'test'));
-	querySnapshot.forEach((doc) => {
-		console.log(`${doc.id}`);
-		const data = doc.data();
-		console.log(data['firstname'], data['surname']);
-	});
-}
-
-server();
+const bot = new TmiService();
+bot.connect();
